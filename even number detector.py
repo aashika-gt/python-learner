52657b89108ee89f4dc12cf8a498e9f6e7ea7f2f
Aashika Gupta <aashika_241345030001@hnbgu.edu.in>
2025-09-26 19:22:13 +0530

    Create even number detector.py
    CHECK YOUR NUMBER IS EVEN OR ODD

---
+++ even number detector.py
@@ -0,0 +1,8 @@
+# CHECK THE NUMBER IS EVEN OR ODD
+print("CHECK THE NUMBER WHETHER IT IS EVEN OR ODD")
+Input=int(input("PLEASE,ENTER YOUR NUMBER"))
+number=Input%2
+if number==0:
+    print("Given number is an even number")
+else:
+    print("Given number is an odd number")
